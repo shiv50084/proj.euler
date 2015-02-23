d2f124ffcfd93850718c7a1b1aaf83951626e079
Stavros Vagionitis <stavros.vagionitis@gmail.com>
2015-02-23 23:44:29 +0000

    Add a template file for creating the C files.

---
+++ prob_template.c
@@ -0,0 +1,64 @@
+/**
+* @file     probxxx.c
+* @author   Stavros Vagionitis
+* @date     xx Mon 2015
+* @brief    [Problem xxx in Project Euler](https://projecteuler.net/problem=xxx)
+*           Title of problem
+*           ====================
+*           Description of the problem.
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * \brief Check if a number is multiple of another number.
+ *
+ * \param   num     The number to check if it's multiple
+ * \param   mult    The multiple to be checked.
+ * \return  1 if it's multiple, 0 if not.
+ */
+uint8_t isMultiple(uint64_t num, uint64_t mult)
+{
+    if (num % mult == 0)
+        return 1;
+    else
+        return 0;
+}
+
+/**
+ * \brief The solution using a naive way.
+ *
+ * \param   num  Number to do something.
+ * \return  1 if everything is ok, 0 if not.
+ */
+uint8_t solution1(uint64_t num)
+{
+
+    return 1;
+}
+
+/**
+ * \brief Usage of this program.
+ *
+ * \param   argv0   The first argument string which is the current executable.
+ * \return  -1.
+ */
+int usage(char *argv0)
+{
+    fprintf(stdout, "%s <Number to do something.>\n", argv0);
+    return -1;
+}
+
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+        return usage(argv[0]);
+
+    uint64_t num = strtoull(argv[1], NULL, 10);
+
+    solution1(num);
+
+    return 0;
+}
