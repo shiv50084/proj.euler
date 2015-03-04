a789d0b6947a98e17cf2c375b5b6e941f4c26455
Stavros Vagionitis <stavros.vagionitis@gmail.com>
2015-03-04 01:19:44 +0000

    big number library: Introduce the multiplication, not fully implemented yet.

--- bnumber.c
+++ bnumber.c
@@ -240,8 +240,114 @@ uint8_t bnum_add(const bnum_t bn1, const bnum_t bn2, bnum_t *bn)
     return 1;
 }
 
+uint8_t bnum_mult(const bnum_t bn1, const bnum_t bn2, bnum_t *bn)
+{
+    uint8_t *tmp = NULL;
+    bn->num = NULL;
+    bn->sz_num = 0;
+
+    uint64_t max_sz_num = max(bn1.sz_num, bn2.sz_num);
+    uint64_t min_sz_num = min(bn1.sz_num, bn2.sz_num);
+
+    bnum_t max_num = max_sz_bnum(bn1, bn2);
+    bnum_t min_num = min_sz_bnum(bn1, bn2);
+
+    // Allocate bnum to add after multiply
+    // Usually we multiply like the following
+    //      9999    max_num
+    //    x   99    min_num
+    //   -------
+    //     89991
+    //   +89991
+    //   -------
+    //    989901
+    //
+    // In the above example we have 2 bnums which is the
+    // same as the size of the small number.
+    bnum_t *bnum_mult_add = malloc(min_sz_num * sizeof(bnum_t));
+    if (bnum_mult_add == NULL)
+    {
+        fprintf(stderr, "%s:%d Mem Alloc failed\n", __func__, __LINE__);
+        return 0;
+    }
+
+    // Looping through the digits of min number
+    for (uint64_t j = min_sz_num - 1;j != (uint64_t)(-1);j--)
+    {
+        uint64_t carry = 0;
+
+        // The size of the number which produced when multiplying
+        // the digits of the min number with the max number. The size
+        // most of the times will be max_sz_num, but if there is a carry
+        // at the last digit, then there will be max_sz_num+1.
+        bnum_mult_add[j].sz_num = max_sz_num;
+        bnum_mult_add[j].num = malloc(bnum_mult_add[j].sz_num * sizeof(uint8_t));
+        if (bnum_mult_add[j].num == NULL)
+        {
+            fprintf(stderr, "%s:%d Mem Alloc failed\n", __func__, __LINE__);
+            return 0;
+        }
+
+        // Looping through the digits of the max number
+        for (uint64_t i = max_sz_num - 1;i != (uint64_t)(-1);i--)
+        {
+            uint64_t tmp_mult = (min_num.num[j] * max_num.num[i]) + carry;
+
+            if (tmp_mult > 9)
+            {
+                bnum_mult_add[j].num[max_sz_num - 1 - i] = tmp_mult % 10;
+                carry = tmp_mult / 10;
+
+                // There is a carry at the last digit, the size of
+                // max_sz_num is becoming max_sz_num+1;
+                if (i == 0)
+                {
+                    bnum_mult_add[j].sz_num += 1;
+
+                    tmp = realloc(bnum_mult_add[j].num,bnum_mult_add[j].sz_num  * sizeof(uint8_t));
+                    if (tmp != NULL)
+                        bnum_mult_add[j].num = tmp;
+                    else
+                    {
+                        fprintf(stderr, "%s:%d Mem Alloc failed\n", __func__, __LINE__);
+
+                        if (bnum_mult_add[j].num)
+                            free(bnum_mult_add[j].num);
+
+                        return 0;
+                    }
+
+                    bnum_mult_add[j].num[bnum_mult_add[j].sz_num - 1] = carry;
+                }
+            }
+            else
+            {
+                bnum_mult_add[j].num[max_sz_num - 1 - i] = tmp_mult;
+                carry = 0;
+            }
+        }
+
+        // Reverse digits of the big number
+        for (uint64_t m = 0;m<bnum_mult_add[j].sz_num / 2;m++)
+        {
+            bnum_mult_add[j].num[m] ^= bnum_mult_add[j].num[bnum_mult_add[j].sz_num - 1 - m];
+            bnum_mult_add[j].num[bnum_mult_add[j].sz_num - 1 - m] ^= bnum_mult_add[j].num[m];
+            bnum_mult_add[j].num[m] ^= bnum_mult_add[j].num[bnum_mult_add[j].sz_num - 1 - m];
+        }
+
+#if 1
+        for (uint64_t k = 0;k<bnum_mult_add[j].sz_num;k++)
+            fprintf(stdout, "%ju", bnum_mult_add[j].num[k]);
+        fprintf(stdout, "\n");
+#endif
+
+
+    }
+}
+
 int main(int argc, char *arv[])
 {
+    // Convert number to bnum
     bnum_t b;
     convert_num_to_bnum(123455, &b);
 
@@ -254,7 +360,7 @@ int main(int argc, char *arv[])
         free(b.num);
 
 
-
+    // Convert string to bnum
     bnum_t c;
     convert_string_to_bnum("543211", &c);
 
@@ -267,12 +373,12 @@ int main(int argc, char *arv[])
         free(c.num);
 
 
+    // Addition
     bnum_t b1, b2, add;
 
     convert_num_to_bnum(999999999, &b1);
     convert_num_to_bnum(99999, &b2);
 
-    fprintf(stdout, "Adding...\n");
     bnum_add(b1, b2, &add);
 
     for (uint64_t i = 0;i<add.sz_num;i++)
@@ -280,5 +386,32 @@ int main(int argc, char *arv[])
 
     fprintf(stdout, "\n");
 
+    if (b1.num)
+        free(b1.num);
+    if (b2.num)
+        free(b2.num);
+    if (add.num)
+        free(add.num);
+
+
+    // Multiplication
+    bnum_t b3, b4, mult;
+
+    convert_num_to_bnum(9999, &b3);
+    convert_num_to_bnum(99, &b4);
+
+    bnum_mult(b3, b4, &mult);
+
+    for (uint64_t i = 0;i<mult.sz_num;i++)
+        fprintf(stdout, "%u", mult.num[i]);
+
+    fprintf(stdout, "\n");
+
+    if (b3.num)
+        free(b3.num);
+    if (b4.num)
+        free(b4.num);
+    if (mult.num)
+        free(mult.num);
 
 }
